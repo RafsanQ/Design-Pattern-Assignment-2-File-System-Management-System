166d61b896176da7cebf978488d5c49d4781bd63
Rafsan <rafsan.quayes@gmail.com>
2021-09-26 22:54:44 +0600

    Created constructors

--- src/File.java
+++ src/File.java
@@ -1,8 +1,17 @@
 public class File implements Component{
 
+    String name, type;
+
+    // The constructor
+    public File(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+
     @Override
     public void singleClick() {
-        
+
     }
 
     @Override
--- src/Folder.java
+++ src/Folder.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+
 public class Folder implements Component{
+
+    ArrayList<Component> components = new ArrayList<>();
+
+    String name, type;
+
+    // The constructor
+    public Folder(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
 
     @Override
     public void singleClick() {
