073795811312849eb4802d31adfaa56af2948944
Rafsan <rafsan.quayes@gmail.com>
2021-09-26 22:50:20 +0600

    Created the Folder and File Classes

---
+++ src/File.java
@@ -0,0 +1,12 @@
+public class File implements Component{
+
+    @Override
+    public void singleClick() {
+        
+    }
+
+    @Override
+    public void doubleClick() {
+
+    }
+}
---
+++ src/Folder.java
@@ -0,0 +1,12 @@
+public class Folder implements Component{
+
+    @Override
+    public void singleClick() {
+
+    }
+
+    @Override
+    public void doubleClick() {
+
+    }
+}
