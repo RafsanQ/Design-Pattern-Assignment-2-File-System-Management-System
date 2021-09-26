8c003eed2318d7fedc99abc9af1062610e2c66f1
Rafsan <rafsan.quayes@gmail.com>
2021-09-26 23:26:51 +0600

    Validation Done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,66 @@
 public class Main {
     public static void main(String[] args) {
-        File file1 = new File("File1", "pdf");
-        file1.singleClick();
 
-        Folder root = new Folder("Root", "Folder");
-        root.singleClick();
+        // File and folder creation
+        File strategy = new File("Strategy Pattern", "ppt");
+        File observer = new File("Observer Pattern", "pdf");
+        File midSyllabus = new File("Mid Syllabus", "doc");
+        File composite = new File("Composite Pattern","ppt");
+        File finalSyllabus = new File("Final Syllabus","pdf");
+        File assignment1 = new File("Assignment-1","pdf");
+        File assignment2 = new File("Assignment-2","pdf");
+        File readME = new File("readME","text");
 
-        root.add(file1);
+        Folder midExam = new Folder("Mid Exam", "Folder");
+        Folder finalExam = new Folder("Final Exam","Folder");
+        Folder Lectures = new Folder("Lectures", "Folder");
+        Folder assignments = new Folder("Assignments", "Folder");
+        Folder DP= new Folder("Design Pattern", "Folder");
 
-        root.doubleClick();
+
+        // Adding the files and checking the output with single and double clicks.
+
+        midExam.add(strategy);
+        midExam.add(observer);
+
+        midExam.singleClick();
+        midExam.doubleClick();
+
+        midExam.add(midSyllabus);
+        finalExam.add(composite);
+        finalExam.add(finalSyllabus);
+        Lectures.add(midExam);
+        Lectures.add(finalExam);
+        assignments.add(assignment1);
+        assignments.add(assignment2);
+        DP.add(Lectures);
+        DP.add(readME);
+        DP.add(assignments);
+
+        midExam.singleClick();
+        midExam.doubleClick();
+
+
+        midExam.add(midSyllabus);
+        finalExam.add(composite);
+        finalExam.add(finalSyllabus);
+        Lectures.add(midExam);
+        Lectures.add(finalExam);
+        assignments.add(assignment1);
+        assignments.add(assignment2);
+        DP.add(Lectures);
+        DP.add(readME);
+        DP.add(assignments);
+
+        DP.singleClick();
+        DP.doubleClick();
+
+        // Remove readme and check the contents again
+        DP.remove(readME);
+
+        DP.doubleClick();
+
+
+
     }
 }
